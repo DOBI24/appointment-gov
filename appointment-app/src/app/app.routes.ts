9d6b5aa02932a8ec860dab51127df0e8c64074ed
Marton Pál <martonpal00@gmail.com>
2024-05-05 05:36:56 +0200

    generate shared components

--- appointment-app/src/app/app.routes.ts
+++ appointment-app/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { MainComponent } from './pages/main/main.component';
 
 export const routes: Routes = [
+    { path: 'main', component: MainComponent },
+    { path: '',   redirectTo: '/main', pathMatch: 'full' },
     { path: '**', component: PageNotFoundComponent }
 ];
