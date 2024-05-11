9b988b4c135946d1e596a40f4270b53ed0532ad3
Marton Pál <martonpal00@gmail.com>
2024-05-12 01:52:37 +0200

    Install firebase

--- appointment-app/src/app/app.config.ts
+++ appointment-app/src/app/app.config.ts
@@ -1,10 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"dobi-appointment-app","appId":"1:2110405123:web:545864240d59a984636c36","storageBucket":"dobi-appointment-app.appspot.com","apiKey":"AIzaSyDZiRRbCcCsXobYyVw5PlXLy1bGygU1a0g","authDomain":"dobi-appointment-app.firebaseapp.com","messagingSenderId":"2110405123","measurementId":"G-7PGX8FVCFF"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage()))]
 };
