8378812f4324ed068dd4124bef79da73882c2647
Marton Pál <martonpal00@gmail.com>
2024-05-12 04:00:33 +0200

    create book component

--- appointment-app/src/app/app.routes.ts
+++ appointment-app/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.com
 import { MainComponent } from './pages/main/main.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { BookComponent } from './pages/book/book.component';
 
 export const routes: Routes = [
     { path: 'main', component: MainComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'book', component: BookComponent },
     { path: '',   redirectTo: '/main', pathMatch: 'full' },
     { path: '**', component: PageNotFoundComponent }
 ];
--- appointment-app/src/app/pages/book/book.component.ts
+++ appointment-app/src/app/pages/book/book.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatInput } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-book',
@@ -12,13 +15,15 @@ import { MatInput } from '@angular/material/input';
     MatFormFieldModule,
     ReactiveFormsModule,
     MatInput,
+    MatSelectModule,
+    MatButtonModule
   ],
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss'
 })
 export class BookComponent {
   basicFormGroup = new FormGroup({
-    email : new FormControl('', [Validators.required, Validators.email]),
-    password : new FormControl('', [Validators.required, Validators.minLength(8)]),
+    name : new FormControl('', [Validators.required, Validators.pattern("[A-Za-z]+ [A-Za-z ]+")]),
+    case : new FormControl('', [Validators.required])
   })
 }
