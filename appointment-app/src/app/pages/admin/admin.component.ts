6c41d660b4f2332f449befbfa84f49c803285d04
Marton Pál <martonpal00@gmail.com>
2024-05-19 07:28:10 +0200

    Appointment list 1.0

--- appointment-app/src/app/pages/admin/admin.component.ts
+++ appointment-app/src/app/pages/admin/admin.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
 import { UserListComponent } from "../../shared/components/user-list/user-list.component";
+import { AppointmentListComponent } from "../../shared/components/appointment-list/appointment-list.component";
 
 @Component({
     selector: 'app-admin',
     standalone: true,
     templateUrl: './admin.component.html',
     styleUrl: './admin.component.scss',
-    imports: [MatTabsModule, UserListComponent]
+    imports: [MatTabsModule, UserListComponent, AppointmentListComponent]
 })
 export class AdminComponent {
 
--- appointment-app/src/app/shared/components/appointment-list/appointment-list.component.ts
+++ appointment-app/src/app/shared/components/appointment-list/appointment-list.component.ts
@@ -1,12 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Appointment } from '../../model/appointment';
+import { Subscription } from 'rxjs';
+import { AppointmentService } from '../../services/appointment.service';
+import { MatTableModule } from '@angular/material/table';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { DatePipe } from '@angular/common';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { Case } from '../../model/case';
+import { CaseService } from '../../services/case.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-appointment-list',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatTableModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    DatePipe,
+    MatDatepickerModule
+  ],
+  providers: [
+    provideNativeDateAdapter(),
+  ],
   templateUrl: './appointment-list.component.html',
   styleUrl: './appointment-list.component.scss'
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit, OnDestroy {
+  appointments: Appointment[] = new Array<Appointment>();
+  appointmentServiceSubscription?: Subscription;
+  displayedColumns: string[] = ['name', 'case', 'date', 'action'];
+  editedAppointment?: Appointment;
+  
+  cases: Case[];
+  caseSubscriptions: Subscription;
 
+  editedAppointmentFormGroup = new FormGroup({
+    name: new FormControl('', [Validators.pattern("[A-Za-z]+ [A-Za-z ]+")]),
+    case: new FormControl(''),
+    date: new FormControl(new Date()),
+  })
+
+  constructor(private appointmentService: AppointmentService, private caseService: CaseService, private snackBar: MatSnackBar) { }
+  ngOnInit(): void {
+    this.appointmentServiceSubscription = this.appointmentService.getAllAppointments().subscribe((appointments) => {
+      this.appointments = appointments;
+    });
+    this.caseSubscriptions = this.caseService.getAllCases().subscribe((cases) => {
+      this.cases = cases;
+    });
+  }
+  
+  ngOnDestroy(): void {
+    this.appointmentServiceSubscription?.unsubscribe();
+    this.caseSubscriptions.unsubscribe();
+  }
+
+  edit(appointment: Appointment) {
+    if (this.editedAppointment) this.exitEdit();
+
+    this.editedAppointment = appointment;
+    this.editedAppointmentFormGroup.get('name')?.setValue(appointment.name);
+    this.editedAppointmentFormGroup.get('case')?.setValue(appointment.case);
+    this.editedAppointmentFormGroup.get('date')?.setValue(appointment.date.toDate());
+  }
+
+  deleteAppointment(appointment: Appointment) {
+    this.appointmentService.deleteAppointmentByID(appointment.id as string).then(_ => {
+      this.snackBar.open('Sikeres törlés', 'Undo',{
+        duration: 3000
+      });
+    });
+  }
+
+  save(appointment: Appointment) {
+    throw new Error('Method not implemented.');
+  }
+
+  exitEdit() {
+    this.editedAppointment = undefined;
+  }
 }
