f96e237d0f0b3a60185c963108cf78db99f1cd6c
Marton Pál <martonpal00@gmail.com>
2024-05-19 02:20:57 +0200

    admin guard

--- appointment-app/src/app/shared/services/admin.guard.ts
+++ appointment-app/src/app/shared/services/admin.guard.ts
@@ -1,12 +1,12 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 import { inject } from '@angular/core';
 
 export const adminGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
-  
-  console.log(authService.user);
-  
-  if (authService.user) return true;
+  const router = inject(Router);
+
+  if (authService.user && authService.user?.role === "admin") return true;
+  router.navigateByUrl("/main");
   return false;
 };
